e8a940721b013cc59534114df1bcdc39200e2039
Eric <leungkachiiiii@gmail.com>
2020-11-06 12:05:08 -0800

    contact font responsive
    Signed-off-by: Eric <leungkachiiiii@gmail.com>

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,7 +4,10 @@ function Contact() {
   return (
     <div className="contact">
       <h4>Open to Work</h4>
-      <a className="email" href="mailto:vjcliang@ucdavis.edu">vjcliang@ucdavis.edu</a>
+      <a className="email" href="mailto:vjcliang@ucdavis.edu">
+        <span className="line">vjcliang</span>
+        <span className="line">@ucdavis.edu</span>
+      </a>
     </div>
   );
 }
